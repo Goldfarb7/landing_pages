55a5a9df33e27464264bd38964696dd12238aa5b
Goldfarb7 <schuylerdeveloper@gmail.com>
2024-06-21 03:14:38 +0530

    Update botchat.js added phone icon

--- botchat.js
+++ botchat.js
@@ -189,7 +189,7 @@ function createButtons(msg){
     s = ""
     Object.keys(msg).forEach(key => {
         //button value check (for final button value change i.e. alloted no. by ringba)
-        let btnValue = msg[key]['next'] ==="callMe" ? document.getElementById("mobile-number").href.split(":").pop().trim() : msg[key]['text'];
+        let btnValue = msg[key]['next'] ==="callMe" ? "📞&nbsp;" + document.getElementById("mobile-number").href.split(":").pop().trim() : msg[key]['text'];
         //creating button
         s+=`<button onclick="chat('${msg[key]['next']}', inResponseOf='${msg[key]['text']}')" class="${msg[key]['color']} btn">
                ${btnValue}
