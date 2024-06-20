9fa82b575831948f2835f20995d7534d3f7d5808
Goldfarb7 <schuylerdeveloper@gmail.com>
2024-06-21 02:40:25 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,35 @@
+<link href="https://use.typekit.net/mrg0hpc.css" rel="stylesheet">
+  
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16576154023"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'AW-16576154023');
+    </script>
+
+      
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '245121381761846'); 
+        fbq('track', 'PageView');
+        // fbq('track', 'Contact');
+    </script>
+    <noscript>
+      <img height="1" width="1" src="https://www.facebook.com/tr?id=245121381761846&ev=PageView&noscript=1"/>
+    </noscript>
+    <!-- End Meta Pixel Code -->
+
+      
+    <!-- ringba code -->
+    <script src=//b-js.ringba.com/CA11a361139cb24bf189cef7eed1fb8ce4 async></script>
+
