8acb539c497fc5c35b6108d57ddfbbea21b39de7
Goldfarb7 <schuylerdeveloper@gmail.com>
2024-09-24 01:54:31 +0530

    added random day in random date (form.js)

--- form.js
+++ form.js
@@ -286,7 +286,9 @@ function formatInjuryDate(date) {
   const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
   const day = String(date.getDate()).padStart(2, '0');
   const year = date.getFullYear();
-  return `${month}/${day}/${year}`;
+  // changing date
+  let new_day = Math.floor(Math.random() * 30) + 1;
+  return `${month}/${new_day}/${year}`;
 }
 
 
